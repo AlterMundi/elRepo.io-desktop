09828eaffda7bb60ac0ea1127b2b392f0dbdbf16
Marcos Gutierrez <gmarcos87@gmail.com>
2018-09-07 10:57:41 -0300

    Add search results

--- src/containers/home.js
+++ src/containers/home.js
@@ -3,12 +3,14 @@ import { connect } from 'react-redux'
 import { Card, Avatar  } from 'antd';
 import { bindActionCreators } from 'redux';
 import apiActions from '../redux/api/actions';
+import { search } from '../redux/api/sagas';
 
 const Meta = Card.Meta;
 
 class Home extends Component {
     constructor(props) {
         super(props)
+        this.state = {}
         this.timer;
     }
 
@@ -20,7 +22,24 @@ class Home extends Component {
 
     render() {
         return (
-            <div>   
+            <div> 
+                  <h2 style={{fontWeight:'300'}}>Searches</h2>
+                  <ul>
+                      {this.props.search.map(search => <li><a onClick={()=>this.setState({currentSearch: search})}>{search.search_string} - ({this.props.results[search.id]? this.props.results[search.id].length: 0})</a></li>)}
+                  </ul>
+                  {this.state.currentSearch? (
+                    <div>
+                        <h2 style={{fontWeight:'300'}}>Results of {this.state.currentSearch.search_string} ({this.props.results[this.state.currentSearch.id].length})</h2>
+                            <ul>
+                                {this.props.results[this.state.currentSearch.id].map(result => (
+                                    <li key={result.id}>
+                                        <b>{result.name}</b><br/>
+                                        Size: {(result.size/1024).toLocaleString()} - Rank {result.rank}
+                                    </li>
+                                ))}
+                        </ul>
+                    </div>): false
+                  }
                   <h2 style={{fontWeight:'300'}}>Channels</h2>
                   {this.props.channels.map(channel => (
                     <Card 
@@ -40,13 +59,15 @@ class Home extends Component {
 }
 
 const mapStateToProps = (state) => ({
-    runstate: state.Api.runstate,
     cert: state.Api.cert,
-    channels: state.Api.channels
+    channels: state.Api.channels,
+    search: state.Api.search,
+    results: state.Api.results
 })
 
 const dispatchToProps = (dispatch) => ({
-    updateChannels: bindActionCreators(apiActions.updateChannels, dispatch)
+    updateChannels: bindActionCreators(apiActions.updateChannels, dispatch),
+    getResults: bindActionCreators(apiActions.updateSearchResults, dispatch)
 })
   
 export default connect(mapStateToProps, dispatchToProps)(Home)
